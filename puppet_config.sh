dfeced2fa741584f4b5aaea5bb13e4aaad2875d5
Guto Carvalho <gutocarvalho@instruct.com.br>
2016-02-20 16:52:29 -0200

    Ajuste na copia do manifest principal

--- puppet_config.sh
+++ puppet_config.sh
@@ -3,7 +3,7 @@
 mkdir -p puppet/environments/test
 mkdir -p puppet/environments/test/modules
 mkdir -p puppet/environments/test/manifests
-ln -s sei.pp puppet/environments/test/manifests/site.pp
+cp sei.pp puppet/environments/test/manifests/site.pp
 cd puppet/environments/test/modules
 git clone https://bitbucket.org/instruct/puppet-sei-aio seiaio
 puppet module install puppetlabs-apache --modulepath=.
